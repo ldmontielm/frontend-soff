ac9cddb3259d36fa1b71a160e63d355d7a36ed40
carolinacr96 <ccastro599@misena.edu.co>
2023-10-08 12:30:08 -0500

    productsV14

--- app/products/components/view-details-byProduct/ViewDetailsByProduct.tsx
+++ app/products/components/view-details-byProduct/ViewDetailsByProduct.tsx
@@ -1,21 +1,20 @@
 'use client'
 
-import { DetailsRecipe, Product } from '@/app/products/models/product.models'
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import { Input } from "@/components/ui/input"
+import { 
+  Dialog, 
+  DialogContent, 
+  DialogDescription, 
+  DialogFooter, 
+  DialogHeader, 
+  DialogTitle, 
+  DialogTrigger } from "@/components/ui/dialog"
 import { EyeIcon, QueueListIcon } from '@heroicons/react/24/outline'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import toast from 'react-hot-toast'
 import { getDetailsByProductId, getProductById, urlProducts } from '@/app/products/services/products.services'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import useSWR from 'swr'
 import { convertToCOP } from '@/app/products/utils'
-
 import {
     Table,
     TableBody,
@@ -31,6 +30,8 @@ export default function ViewDetailsByProduct({productId}:{productId:string}) {
   const router = useRouter()
   const {data:products}= useSWR(`${urlProducts}/${productId}`,getProductById)
   const {data: details} = useSWR(`${productId}`, getDetailsByProductId)
+  const cost = products?.price || 0
+  const sale_price = products?.sale_price || 0
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -40,60 +41,56 @@ export default function ViewDetailsByProduct({productId}:{productId:string}) {
       </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[600px]">
-          <div className='w-full md:col-span-8'>
-              <div className='w-full text-center mb-1 p-2'>
-                <p className='font-bold'>Detalles del producto</p>
-                <p className='text-sm'>Aquí puedes ver el detalle de cada producto</p>
-              </div>
-              <hr />
-              <div>
+        <div className='w-full md:col-span-8'>
+          <div className='w-full text-center mb-1 p-2'>
+            <p className='font-bold'>Detalles del producto</p>
+            <p className='text-sm'>Aquí puedes ver el detalle de cada producto</p>
+          </div>
+          <hr />
+          <div>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 mt-2'>
               <div className='my-3 w-full text-center'>
                 <p className='font-bold text-sm '>Nombre</p>
-                  <p className='text-sm text-gray-400'>{products?.name}</p>
-                  
-                </div>
-                <div className='my-3 w-full text-center'>
-                  <p className='font-bold text-sm'>Costo</p>
-                  <p className=' text-sm text-gray-400'>${products?.price}</p>
-                  
-                </div>
-                <div className='my-3 w-full text-center'>
+                <p className='text-sm text-gray-400'>{products?.name}</p>  
+              </div>
+              <div className='my-3 w-full text-center'>
+                <p className='font-bold text-sm'>Costo</p>
+                <p className=' text-sm text-gray-400'>${convertToCOP(cost)}</p>
+              </div>
+              <div className='my-3 w-full text-center'>
                 <p className='font-bold text-sm'>Precio de venta</p>
-                  <p className=' text-sm text-gray-400'>${products?.sale_price}</p>
-                  
-                </div>
-                </div>
+                <p className=' text-sm text-gray-400'>${convertToCOP(sale_price)}</p>
               </div>
             </div>
-          <div className='relative w-full bg-white col-span-8 border rounded'>
-          <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Insumo</TableHead>
-                  <TableHead>Cantidad</TableHead>
-                  <TableHead>Unidad de medida</TableHead>
-                  <TableHead>Precio Unitario</TableHead>
-                  <TableHead>Subtotal</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {
-                  Array.isArray(details) && details.map((detail) => (
-                    <TableRow key={detail.supply_id}>
-                      <TableCell className="font-medium capitalize">{detail.supply}</TableCell>
-                      <TableCell>{detail.amount_supply}</TableCell>
-                      <TableCell>{detail.unit_measure}</TableCell>
-                      <TableCell>${detail.supply_price}</TableCell>
-                      <TableCell>${detail.subtotal}</TableCell>
-                    </TableRow>
-                  ))
-                }
-              </TableBody>
-            </Table>
           </div>
-        </DialogContent>
-      </Dialog>
-      
+        </div>
+        <div className='relative w-full bg-white col-span-8 border rounded'>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Insumo</TableHead>
+                <TableHead>Cantidad</TableHead>
+                <TableHead>Unidad de medida</TableHead>
+                <TableHead>Precio Unitario</TableHead>
+                <TableHead>Subtotal</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {
+                Array.isArray(details) && details.map((detail) => (
+                  <TableRow key={detail.supply_id}>
+                    <TableCell className="font-medium capitalize">{detail.supply}</TableCell>
+                    <TableCell>{detail.amount_supply}</TableCell>
+                    <TableCell>{detail.unit_measure}</TableCell>
+                    <TableCell>${convertToCOP(detail.supply_price)}</TableCell>
+                    <TableCell>${convertToCOP(detail.subtotal)}</TableCell>
+                  </TableRow>
+                ))
+              }
+            </TableBody>
+          </Table>
+        </div>
+      </DialogContent>
+    </Dialog>  
   )
 }
