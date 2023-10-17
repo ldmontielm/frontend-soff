7e562170aa2d6f9bda095c84303bf65ba1fb4b27
DIDIER321 <decastaneda949@misena.educo>
2023-10-17 12:58:11 -0500

    user_roles_update

--- app/roles/payments/data-table.tsx
+++ app/roles/payments/data-table.tsx
@@ -27,6 +27,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import {  ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import HeadTable from "../components/head-table/HeadTable"
@@ -164,13 +165,15 @@ export function DataTable<TData, TValue>({
     </div>
 </div>
     {/* ------------------------paginado---------------------------- */}
-    <div className="flex items-center justify-end space-x-2 py-4">
+    <div className="flex items-center justify-lingth space-x-2 py-4">
     <Button
       variant="outline"
       size="sm"
       onClick={() => table.previousPage()}
       disabled={!table.getCanPreviousPage()}
     >
+          <ChevronLeftIcon className='w-4 h-4' />
+
       Anterior
     </Button>
     <Button
@@ -179,8 +182,24 @@ export function DataTable<TData, TValue>({
       onClick={() => table.nextPage()}
       disabled={!table.getCanNextPage()}
     >
-      Siguiente 
+      Proxima
+      <ChevronRightIcon className='w-4 h-4' />
     </Button>
+
+    <select
+          className='w-fit flex h-9 items-center justify-between rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 '
+          value={table.getState().pagination.pageSize}
+          onChange={e => {
+            table.setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Mostrar {pageSize}
+            </option>
+          ))}
+        </select>
+        <p>Página {table.getState().pagination.pageIndex + 1} de {table.getPageCount()}</p>
   </div>
 </div>
   )
