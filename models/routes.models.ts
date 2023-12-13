46429ff74eb02bd273a7c81e2f96cb491285380f
= <=>
2023-12-13 11:34:53 -0500

    routes add

--- models/routes.models.ts
+++ models/routes.models.ts
@@ -11,16 +11,16 @@ export enum Routes{
 }
 
 export enum RoutesApi {
-  SALES = 'http://localhost:8000/sales',
-  PURCHASES = 'http://localhost:8000/purchases',
-  PRODUCTS = 'http://localhost:8000/products',
-  PROVIDERS= 'http://localhost:8000/providers',
-  ROLES = 'http://localhost:8000/roles',
-  SUPPLIES = 'http://localhost:8000/supplies',
-  USERS = 'http://localhost:8000/users',
-  PERMISSIONS = 'http://localhost:8000/permissions',
-  AUTH='http://localhost:8000/auth',
-  DASHBOARD='http://localhost:8000/dashboard'
+  // SALES = 'http://localhost:8000/sales',
+  // PURCHASES = 'http://localhost:8000/purchases',
+  // PRODUCTS = 'http://localhost:8000/products',
+  // PROVIDERS= 'http://localhost:8000/providers',
+  // ROLES = 'http://localhost:8000/roles',
+  // SUPPLIES = 'http://localhost:8000/supplies',
+  // USERS = 'http://localhost:8000/users',
+  // PERMISSIONS = 'http://localhost:8000/permissions',
+  // AUTH='http://localhost:8000/auth',
+  // DASHBOARD='http://localhost:8000/dashboard'
   // SALES = 'http://34.239.207.131/sales',
   // PURCHASES = 'http://34.239.207.131/purchases',
   // PRODUCTS = 'http://34.239.207.131/products',
@@ -31,14 +31,14 @@ export enum RoutesApi {
   // PERMISSIONS = 'http://34.239.207.131/permissions',
   // AUTH='http://34.239.207.131/auth',
   // DASHBOARD='http://34.239.207.131/dashboard'
-  // SALES = 'https://rest-api-soff.vercel.app/sales',
-  // PURCHASES = 'https://rest-api-soff.vercel.app/purchases',
-  // PRODUCTS = 'https://rest-api-soff.vercel.app/products',
-  // PROVIDERS= 'https://rest-api-soff.vercel.app/providers',
-  // ROLES = 'https://rest-api-soff.vercel.app/roles',
-  // SUPPLIES = 'https://rest-api-soff.vercel.app/supplies',
-  // USERS = 'https://rest-api-soff.vercel.app/users',
-  // PERMISSIONS = 'https://rest-api-soff.vercel.app/permissions',
-  // AUTH='https://rest-api-soff.vercel.app/auth',
-  // DASHBOARD='https://rest-api-soff.vercel.app/dashboard'
+  SALES = 'https://rest-api-soff.vercel.app/sales',
+  PURCHASES = 'https://rest-api-soff.vercel.app/purchases',
+  PRODUCTS = 'https://rest-api-soff.vercel.app/products',
+  PROVIDERS= 'https://rest-api-soff.vercel.app/providers',
+  ROLES = 'https://rest-api-soff.vercel.app/roles',
+  SUPPLIES = 'https://rest-api-soff.vercel.app/supplies',
+  USERS = 'https://rest-api-soff.vercel.app/users',
+  PERMISSIONS = 'https://rest-api-soff.vercel.app/permissions',
+  AUTH='https://rest-api-soff.vercel.app/auth',
+  DASHBOARD='https://rest-api-soff.vercel.app/dashboard'
 }
